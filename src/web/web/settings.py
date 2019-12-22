2fa9fea191ff2cd9cf38ef1e6c80d8beed4d6345
Vadim Koziachiy <vadim.koziachiy@exposit.com>
2019-12-22 16:11:04 +0300

    add nginx + uWSGI

--- src/web/web/settings.py
+++ src/web/web/settings.py
@@ -29,7 +29,7 @@ SECRET_KEY = env('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = env('DEBUG')
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['django']
 
 # Application definition
 
@@ -121,6 +121,6 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/2.2/howto/static-files/
 
 STATIC_URL = '/static/'
-
+STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 
 from .logging import *
