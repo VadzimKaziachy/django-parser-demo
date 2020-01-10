cc8f3e1e9856879ffed5e1ff73e326ed6cbd7aed
Vadim Koziachiy <vadim.koziachiy@exposit.com>
2020-01-10 14:10:03 +0300

    added scrapy and scrapyd

---
+++ src/parser/twenty_first_century/pipelines.py
@@ -0,0 +1,22 @@
+# -*- coding: utf-8 -*-
+
+# Define your item pipelines here
+#
+# Don't forget to add your pipeline to the ITEM_PIPELINES setting
+# See: https://docs.scrapy.org/en/latest/topics/item-pipeline.html
+from typing import List, NoReturn
+
+from twenty_first_century.items import ProductItem
+
+
+class TwentyFirstCenterPipeline(object):
+
+    def __init__(self) -> NoReturn:
+        self.products: List[ProductItem] = list()
+
+    def process_item(self, product, spider) -> ProductItem:
+        self.products.append(product)
+        return product
+
+    def close_spider(self, spider) -> NoReturn:
+        pass
---
+++ src/parser/twenty_first_century/spiders/category.py
@@ -0,0 +1,22 @@
+from scrapy.spiders import CrawlSpider, Rule
+from scrapy.linkextractors import LinkExtractor
+
+from twenty_first_century.items import ProductItem
+
+
+class CategorySpider(CrawlSpider):
+    name = 'category'
+    allowed_domains = ['www.www.21vek.by']
+    start_urls = ['https://www.21vek.by/tv/']
+
+    rules = (
+        Rule(LinkExtractor(restrict_css=['a.j-load_page']), callback='parser_page', follow=True),
+    )
+
+    def parser_page(self, response):
+        for product in response.css('span.g-item-data'):
+            yield ProductItem(
+                name=product.attrib.get('data-name', None),
+                code=product.attrib.get('data-code', None),
+                price=product.attrib.get('data-price', None)
+            )
